fca0cf43fa08d5ec96315fc79b01868904c2c6ad
mauriciorios <mauriciorios@knights.ucf.edu>
2021-06-13 20:02:39 -0400

    Test

--- src/test/java/oop/assignment2/ex27/ex27_solutionTest.java
+++ src/test/java/oop/assignment2/ex27/ex27_solutionTest.java
@@ -10,7 +10,7 @@ class ex27_solutionTest {
     void validateInput_test_no_errors_found() {
         ex27_solution app = new ex27_solution();
 
-        String output = app.validateInput("Jared","Higgens","99999","JW-1234");
+        String output = app.validateInput("Jarad","Higgins","99999","JW-1234");
 
         assertEquals("There were no errors found.", output);
     }
@@ -19,7 +19,7 @@ class ex27_solutionTest {
     void validateInput_test_no_input_for_fname() {
         ex27_solution app = new ex27_solution();
 
-        String output = app.validateInput("","Higgens","99999","JW-1234");
+        String output = app.validateInput("","Higgins","99999","JW-1234");
 
         assertEquals("The first name must be filled in.\nThe first name must be at least 2 characters long.\n"
                 ,output);
@@ -29,7 +29,7 @@ class ex27_solutionTest {
     void validateInput_test_wrong_input_for_lname() {
         ex27_solution app = new ex27_solution();
 
-        String output = app.validateInput("Jared","h","99999","JW-1234");
+        String output = app.validateInput("Jarad","h","99999","JW-1234");
 
         assertEquals("The last name must be at least 2 characters long.\n",output);
     }
@@ -38,7 +38,7 @@ class ex27_solutionTest {
     void validateInput_test_wrong_input_for_zipcode() {
         ex27_solution app = new ex27_solution();
 
-        String output = app.validateInput("Jared","Higgens","23df43","JW-1234");
+        String output = app.validateInput("Jarad","Higgins","23df43","JW-1234");
 
         assertEquals("The zipcode must be a 5 digit number.\n",output);
     }
@@ -47,8 +47,26 @@ class ex27_solutionTest {
     void validateInput_test_wrong_input_for_ID() {
         ex27_solution app = new ex27_solution();
 
-        String output = app.validateInput("Jared","Higgens","99999","J1-Ab42");
+        String output = app.validateInput("Jarad","Higgins","99999","J1-Ab42");
 
         assertEquals("The employee ID must be in the format of AA-1234.\n",output);
+    }
+
+    @Test
+    void validateInput_test_no_input_for_anything() {
+        ex27_solution app = new ex27_solution();
+
+        String expected = """
+                The first name must be filled in.
+                The first name must be at least 2 characters long.
+                The last name must be filled in.
+                The last name must be at least 2 characters long.
+                The zipcode must be a 5 digit number.
+                The employee ID must be in the format of AA-1234.
+                """;
+
+        String output = app.validateInput("","","","");
+
+        assertEquals(expected,output);
     }
 }
--- src/test/java/oop/assignment2/ex28/ex28_solutionTest.java
+++ src/test/java/oop/assignment2/ex28/ex28_solutionTest.java
@@ -7,8 +7,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class ex28_solutionTest {
 
     @Test
-    void addNumbers() {
+    void addNumbers_testing_small_numbers() {
         ex28_solution app = new ex28_solution();
 
+        double output = app.addNumbers(1,2,3,4,5);
+
+        assertEquals(15, output);
+    }
+
+    @Test
+    void addNumbers_testing_huge_numbers() {
+        ex28_solution app = new ex28_solution();
+
+        double output = app.addNumbers(1000000,2000000,3000000,40000000,500000);
+
+        assertEquals(4.65E7, output);
+    }
+
+    @Test
+    void addNumbers_testing_decimals() {
+        ex28_solution app = new ex28_solution();
+
+        double output = app.addNumbers(534.2324,23424.23424,3.2342342,4.234232,5.79604);
+
+        assertEquals(23971.731146200003, output);
+    }
+
+    @Test
+    void addNumbers_testing_with_input_as_only_zero() {
+        ex28_solution app = new ex28_solution();
+
+
+        double output = app.addNumbers(0,0,0,0,0);
+
+        assertEquals(0, output);
     }
 }
